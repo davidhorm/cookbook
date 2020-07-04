7f21bd7a6bac3f3fd32a2344463e540dcd6027f8
davidhorm <davidhorm+github@gmail.com>
2020-07-04 11:06:37 -0700

    Add initial <Stepper /> to <SectionNav />

--- src/components/RecipeLayout/InstructionsTab/SectionNav/index.tsx
+++ src/components/RecipeLayout/InstructionsTab/SectionNav/index.tsx
@@ -1,10 +1,26 @@
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Stepper from '@material-ui/core/Stepper';
 import React from 'react';
 
+const activeStep = 0;
+
+const navigateToHeaderId = (headerId: string) => console.log(headerId);
+
 type props = {
+  /** List of header ids. */
   headerIds: string[];
 };
-
-// TODO: https://material-ui.com/components/steppers/
-const SectionNav = ({ headerIds }: props) => <nav>{headerIds.map((value) => value)}</nav>;
+const SectionNav = ({ headerIds }: props) => (
+  <nav>
+    <Stepper activeStep={activeStep} orientation="vertical">
+      {headerIds.map((headerId) => (
+        <Step key={headerId}>
+          <StepLabel onClick={() => navigateToHeaderId(headerId)}></StepLabel>
+        </Step>
+      ))}
+    </Stepper>
+  </nav>
+);
 
 export { SectionNav };
